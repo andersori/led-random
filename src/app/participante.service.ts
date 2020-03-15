3b2e2562a8d446523e986290d68afd20390b7973
Anderson Soriano <19699525+andersori@users.noreply.github.com>
2020-03-14 22:18:53 -0300

    touppercase na secret

--- src/app/participante.service.ts
+++ src/app/participante.service.ts
@@ -18,12 +18,12 @@ export class ParticipanteService {
   constructor(private http: HttpClient) { }
 
   public async getParticipante(id: Number, secret: String): Promise<Participante> {
-    return await this.http.get<Participante>(environment.apiUrl + `/public/participant/${id}?secret=${secret.toUpperCase}`)
+    return await this.http.get<Participante>(environment.apiUrl + `/public/participant/${id}?secret=${secret.toUpperCase()}`)
     .toPromise();
   }
 
   public async randomEquipeParti(id: Number, secret: String): Promise<Participante> {
-    return await this.http.post<Participante>(environment.apiUrl + `/public/participant/${id}/random?secret=${secret.toUpperCase}`, null)
+    return await this.http.post<Participante>(environment.apiUrl + `/public/participant/${id}/random?secret=${secret.toUpperCase()}`, null)
     .toPromise();
   }
 }
